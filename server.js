695de85e998b9a42471c3d45282374c410303086
SweetNAMese <t.lyna.nguyen@gmail.com>
2016-04-02 17:10:02 -0400

    Storing Data on the Server Side (a.k.a. Back End) Using HTTP Post json body-parser

--- server.js
+++ server.js
@@ -3,21 +3,32 @@
 
 // require express Library
 var express = require('express');
+// Require body-parser needed by json to auto-parse 'body' in (req.body)
+var bodyParser = require('body-parser');
 // Back-end app
 var app = express();
 
-// Serving static files in Express, lets you specify a folder to serve file
-app.use(express.static('public'));
-
-// API (Application Interface Call), route
-app.get('/meows', function(req, res, next) {
+// Require body-parser needed by json to auto-parse 'body' in (req.body)
+app.use(bodyParser.json());
   var meows = [
     'Hello, I flipped over a cup.',
     'My owner just said hi to me. Yum.',
     'It is time for a nap',
     'It is my birthday!',
   ];
+
+// Serving static files in Express, lets you specify a folder to serve file
+app.use(express.static('public'));
+
+// API (Application Interface Call), route
+app.get('/meows', function(req, res, next) {
+
   return res.send(meows);
+});
+
+app.post('/meows', function(req, res, next) {
+  meows.push(req.body.newMeow);
+  res.send();
 });
 
 // Have to specify port 3000, defaults on 8080. Listen is a callback
