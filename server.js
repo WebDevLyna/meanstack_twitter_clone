5b06bc9a93ba5118e08313de882845eddbf1486d
SweetNAMese <t.lyna.nguyen@gmail.com>
2016-04-02 16:30:01 -0400

    Creating a JS API Route on tther Server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // Using Node js to create this server
+// Note: in CLI enter npm install -g nodemon to allow page refresh w/o restarting server
 
 // require express Library
 var express = require('express');
@@ -7,6 +8,17 @@ var app = express();
 
 // Serving static files in Express, lets you specify a folder to serve file
 app.use(express.static('public'));
+
+// API (Application Interface Call), route
+app.get('/meows', function(req, res, next) {
+  var meows = [
+    'Hello, I flipped over a cup.',
+    'My owner just said hi to me. Yum.',
+    'It is time for a nap',
+    'It is my birthday!',
+  ];
+  return res.send(meows);
+});
 
 // Have to specify port 3000, defaults on 8080. Listen is a callback
 app.listen(3000, function () {
